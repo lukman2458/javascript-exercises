39693b50eb1d925c0a332520a925fafeac8b5041
lukman2458 <@lukman2458.com>
2026-06-28 00:26:06 +0530

    Practise 5 exercises for data types

--- foundations/02_addNumbers/addNumbers.js
+++ foundations/02_addNumbers/addNumbers.js
@@ -4,10 +4,12 @@ function addNumbers() {
 
   let result;
 
-  result = "a" + "b"; // <------ EDIT THIS LINE
+  result = a + b; // <------ EDIT THIS LINE
 
   return result;
+  
 }
+console.log(addNumbers())
 
 // Do not change this
 module.exports = addNumbers;
--- foundations/03_numberChecker/numberChecker.js
+++ foundations/03_numberChecker/numberChecker.js
@@ -1,10 +1,13 @@
 function numberChecker(number) {
-  if (number === 6) {
+  if (number >= 10) {
     return true;
+    console.log("true")
   } else {
     return false;
+    console.log("false")
   }
 }
-
+numberChecker(10)
+console.log(numberChecker())
 // Do not edit below this line
 module.exports = numberChecker;
